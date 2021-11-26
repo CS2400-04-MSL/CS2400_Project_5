6b0391bf82c62501f237de5703260536407da906
Sulfurous-Impersonation <89325475+Sulfurous-Impersonation@users.noreply.github.com>
2021-11-26 14:55:25 -0800

    added Node class to directed graph
    Node class has array of nodes as next instead of only one next

--- GraphPackage/DirectedGraph.java
+++ GraphPackage/DirectedGraph.java
@@ -17,6 +17,12 @@ public class DirectedGraph<T> implements GraphInterface<T>
         return null;
     }
 
+    public QueueInterface<T> getDepthFirstTraversal(T origin) 
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
     public void clear() 
     {
         // TODO Auto-generated method stub
@@ -71,15 +77,46 @@ public class DirectedGraph<T> implements GraphInterface<T>
         return 0;
     }
 
-    public QueueInterface<T> getDepthFirstTraversal(T origin) 
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
     public double getCheapestPath(T begin, T end, StackInterface<T> path) 
     {
         // TODO Auto-generated method stub
         return 0;
     }
+
+    private class Node
+	{
+      private T    data; // Entry in stack
+      private Node[] next; // Link to next node
+      
+      private Node(T dataPortion)
+      {
+         this(dataPortion, null);
+      } // end constructor
+      
+      private Node(T dataPortion, Node[] linkPortion)
+      {
+         data = dataPortion;
+         next = linkPortion;
+      } // end constructor
+      
+      private T getData()
+      {
+         return data;
+      } // end getData
+      
+      private void setData(T newData)
+      {
+         data = newData;
+      } // end setData
+      
+      private Node[] getNextNode()
+      {
+         return next;
+      } // end getNextNode
+      
+      private void setNextNode(Node[] nextNode)
+      {
+         next = nextNode;
+      } // end setNextNode
+	} // end Node
 }
