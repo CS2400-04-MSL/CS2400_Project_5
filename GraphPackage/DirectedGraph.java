ba4e7f15a673c24b96f281541a641b1706b42c78
Sulfurous-Impersonation <89325475+Sulfurous-Impersonation@users.noreply.github.com>
2021-11-26 14:59:45 -0800

    removed unnecessary class, imports ADTPackage.*

--- GraphPackage/DirectedGraph.java
+++ GraphPackage/DirectedGraph.java
@@ -1,7 +1,5 @@
 package GraphPackage;
-
-import ADTPackage.QueueInterface;
-import ADTPackage.StackInterface;
+import ADTPackage.*;
 
 public class DirectedGraph<T> implements GraphInterface<T> 
 {
@@ -20,6 +18,7 @@ public class DirectedGraph<T> implements GraphInterface<T>
     public QueueInterface<T> getDepthFirstTraversal(T origin) 
     {
         // TODO Auto-generated method stub
+        LinkedStack depthStack = new LinkedStack();
         return null;
     }
 
@@ -82,41 +81,4 @@ public class DirectedGraph<T> implements GraphInterface<T>
         // TODO Auto-generated method stub
         return 0;
     }
-
-    private class Node
-	{
-      private T    data; // Entry in stack
-      private Node[] next; // Link to next node
-      
-      private Node(T dataPortion)
-      {
-         this(dataPortion, null);
-      } // end constructor
-      
-      private Node(T dataPortion, Node[] linkPortion)
-      {
-         data = dataPortion;
-         next = linkPortion;
-      } // end constructor
-      
-      private T getData()
-      {
-         return data;
-      } // end getData
-      
-      private void setData(T newData)
-      {
-         data = newData;
-      } // end setData
-      
-      private Node[] getNextNode()
-      {
-         return next;
-      } // end getNextNode
-      
-      private void setNextNode(Node[] nextNode)
-      {
-         next = nextNode;
-      } // end setNextNode
-	} // end Node
 }
